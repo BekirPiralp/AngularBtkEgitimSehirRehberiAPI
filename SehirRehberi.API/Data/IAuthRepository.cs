c3a902e4b1e83843e24522cba0c2f4cf36e24ad0
Bekir PİRALP <bpir33@gmail.com>
2022-08-14 22:42:34 +0300

    Auth deposu kayıt için uygulandı

---
+++ SehirRehberi.API/Data/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using SehirRehberi.API.Model;
+
+namespace SehirRehberi.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user,string password);
+        Task<User> Login(string userName, string password);
+        Task<User> UserExits(string userName);
+    }
+}
