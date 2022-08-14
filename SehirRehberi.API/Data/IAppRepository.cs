cc83de44f6a51ad5b77b26cd41ed5b5dc80e549c
Bekir PİRALP <bpir33@gmail.com>
2022-08-14 11:57:46 +0300

    kiler yapısı uygulandı ;D

---
+++ SehirRehberi.API/Data/IAppRepository.cs
@@ -0,0 +1,16 @@
+﻿using SehirRehberi.API.Model;
+
+namespace SehirRehberi.API.Data
+{
+    public interface IAppRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveAll(); //unit of work tasarım kalıbını uygulamak için kattık
+
+        List<City> GetCities();
+        List<Photo> GetPhotosByCityId(int cityId);
+        City GetCityById(int cityId);
+        Photo GetPhotoById(int photoId);
+    }
+}
---
+++ SehirRehberi.API/Extensions/Menus.cs
@@ -0,0 +1,12 @@
+﻿using SehirRehberi.API.Data;
+
+namespace SehirRehberi.API.Extensions
+{
+    public static class Menus
+    {
+        public static void TheFoodMenu(this IServiceCollection services)
+        {
+            services.AddScoped<IAppRepository, AppRepository>();
+        }
+    }
+}
