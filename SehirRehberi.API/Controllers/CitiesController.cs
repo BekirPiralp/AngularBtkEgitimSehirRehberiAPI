8c7c5ece72b89ef32dbbfba0ab4d88a9d2d44385
Bekir PİRALP <bpir33@gmail.com>
2022-08-14 12:59:04 +0300

    Şehitler için veriyi istenen hale dönüştürmek için bir dönüt yazıldı ve bu dönüte çevirme yapıldı lakin bu çevirme yapılırken dönütler için otomatik bir haritalama ihtiyacı gerekli olduğu ortaya çıkmıştır

---
+++ SehirRehberi.API/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SehirRehberi.API.Data;
+using SehirRehberi.API.DTOS;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private IAppRepository _repository;
+        public CitiesController(IAppRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult GetCities()
+        {
+            var dönüt = _repository.GetCities().Select(
+                c => new CityForListDto
+                {
+                    Description = c.Description,
+                    Name = c.Name,
+                    Id = c.Id,
+                    PhotoUrl = c.Photos.FirstOrDefault(p => p.IsMain == true)
+                                .Url
+                }
+                ).ToList();
+            return Ok(dönüt);
+        }
+    }
+}
---
+++ SehirRehberi.API/DTOS/CityForListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SehirRehberi.API.DTOS
+{
+    public class CityForListDto
+    {
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
