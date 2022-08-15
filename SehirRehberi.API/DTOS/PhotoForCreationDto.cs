421f67aeab01d4332a4844f670ca21b9d18df611
Bekir PİRALP <bpir33@gmail.com>
2022-08-15 22:52:18 +0300

    resimler için dto oluşturduk.

---
+++ SehirRehberi.API/DTOS/PhotoForCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SehirRehberi.API.DTOS
+{
+    public class PhotoForCreationDto
+    {
+        public PhotoForCreationDto()
+        {
+            DateAdded = DateTime.Now;
+        }
+        public IFormFile File { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string PublicId { get; set; }
+    }
+}
